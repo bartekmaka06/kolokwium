ca7f127904630c76297c75ce313d78380a497478
student <228805@edu.p.lodz.pl>
2022-05-16 17:26:34 +0200

    added mock

--- testsReactor/src/test/java/edu/iis/mto/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/coffee/CoffeeMachineTest.java
@@ -1,18 +1,29 @@
 package edu.iis.mto.coffee;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
 
+import edu.iis.mto.coffee.machine.CoffeeGrinder;
+import edu.iis.mto.coffee.machine.MilkProvider;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class CoffeeMachineTest {
 
+    @Mock
+    private CoffeeGrinder grinder;
+    @Mock
+    private MilkProvider milkProvider;
+    @Mock
+    private CoffeeReceipes receipes;
+
     @Test
     void itCompiles() {
-        MatcherAssert.assertThat(true, equalTo(true));
+        assertEquals(true,true);
     }
 
 }
